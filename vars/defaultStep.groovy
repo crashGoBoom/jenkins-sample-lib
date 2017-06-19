bc5fded34198077e04b91671df45d494e87fdb17
crashGoBoom <chris.mundus@gmail.com>
2017-06-19 11:12:28 -0400

    Testing importing tool in lib.

--- vars/defaultStep.groovy
+++ vars/defaultStep.groovy
@@ -3,8 +3,8 @@ def call(name = 'default', args = 'install') {
     // now build/test/whatever based on the info provided
     echo "Building ${name} with these args: ${args}"
     git url: "https://github.com/spring-guides/gs-maven.git"
+    // Lets run this with the global tool "maven".
     withEnv(["PATH+MAVEN=${tool 'maven'}/bin"]) {
        sh "mvn ${args}"
     }
 }
-
