ba11847bd34d9fa9f8aaee8119f1c90df1fef3a3
crashGoBoom <chris.mundus@gmail.com>
2017-06-19 08:10:38 -0400

    Changed to a node.

--- vars/testStage.groovy
+++ vars/testStage.groovy
@@ -1,18 +1,10 @@
 // vars/testStage.groovy
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
+def call(Closure body) {
     // now build/test/whatever, based on the configuration provided
-    stage('hidden stage') {
-	steps{
-		echo "building with this args: ${config.name}"
-		git url: "https://github.com/spring-guides/gs-maven.git"
-		sh "mvn install"
-	}
+    node('hidden stage') {
+	echo "building with this args: ${body.name}"
+	git url: "https://github.com/spring-guides/gs-maven.git"
+	sh "mvn install"
     }
 }
 
