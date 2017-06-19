41d7993926001cfa62b6ccf857e4b2e53e4eda56
crashGoBoom <chris.mundus@gmail.com>
2017-06-19 08:56:00 -0400

    Added args to mvn line.

--- vars/testStage.groovy
+++ vars/testStage.groovy
@@ -1,8 +1,8 @@
 // vars/testStage.groovy
-def call(name = 'default', args = '') {
+def call(name = 'default', args = 'install') {
     // now build/test/whatever based on the info provided
     echo "Building ${name} with these args: ${args}"
     git url: "https://github.com/spring-guides/gs-maven.git"
-    sh "mvn install"
+    sh "mvn ${args}"
 }
 
