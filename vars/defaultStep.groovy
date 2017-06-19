667b3286017917c86558ba4f0c8a82a261a9b1af
crashGoBoom <chris.mundus@gmail.com>
2017-06-19 11:08:38 -0400

    Testing adding tool in lib.

--- vars/defaultStep.groovy
+++ vars/defaultStep.groovy
@@ -3,6 +3,8 @@ def call(name = 'default', args = 'install') {
     // now build/test/whatever based on the info provided
     echo "Building ${name} with these args: ${args}"
     git url: "https://github.com/spring-guides/gs-maven.git"
-    sh "mvn ${args}"
+    withEnv(["PATH+MAVEN=${tool 'maven'}/bin"]) {
+       sh "mvn ${args}"
+    }
 }
 
