44e8c2ff067ba153b5ead94fc44fcdf30bd26fc4
crashGoBoom <chris.mundus@gmail.com>
2017-06-19 08:28:02 -0400

    Modified syntax to simplify.

--- vars/testStage.groovy
+++ vars/testStage.groovy
@@ -1,10 +1,9 @@
 // vars/testStage.groovy
-def call(Closure body) {
-    // now build/test/whatever, based on the configuration provided
-    node('hidden stage') {
-	echo "building with this args: ${body.name}"
-	git url: "https://github.com/spring-guides/gs-maven.git"
-	sh "mvn install"
-    }
+def call(String name = 'default',
+         String args = '') {
+    // now build/test/whatever based on the info provided
+    echo "Building ${name} with these args: ${args}"
+    git url: "https://github.com/spring-guides/gs-maven.git"
+    sh "mvn install"
 }
 
