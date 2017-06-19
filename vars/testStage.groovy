63792c7e6f6694ebe356f7e39c54ee17b9fa3f8a
crashGoBoom <chris.mundus@gmail.com>
2017-06-19 08:51:50 -0400

    Removed String.

--- vars/testStage.groovy
+++ vars/testStage.groovy
@@ -1,6 +1,5 @@
 // vars/testStage.groovy
-def call(String name = 'default',
-         String args = '') {
+def call(name = 'default', args = '') {
     // now build/test/whatever based on the info provided
     echo "Building ${name} with these args: ${args}"
     git url: "https://github.com/spring-guides/gs-maven.git"
