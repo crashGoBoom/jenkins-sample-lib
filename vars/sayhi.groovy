36e5092557cce8bbbcdf624ad5cfc4712789b06d
crashGoBoom <chris.mundus@gmail.com>
2017-06-27 14:04:09 -0400

    Testing object response.

--- vars/sayhi.groovy
+++ vars/sayhi.groovy
@@ -1,3 +1,9 @@
-def call(pipeline_name) {
-    echo "Oh hi...this came from pipeline: ${pipeline_name}."
+class Result {
+   boolean success
+   String reason
+}
+
+Result call(pipeline_name) {
+   echo "Oh hi...this came from pipeline: ${pipeline_name}."
+   return new Result(success: true, reason: "Because it worked.")
 }
