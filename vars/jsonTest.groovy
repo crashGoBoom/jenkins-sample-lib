50c51a873e1bca80e6897dc58a8c6f4ca091c0e5
crashGoBoom <chris.mundus@gmail.com>
2017-06-27 15:18:46 -0400

    Modifed class.

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -1,9 +1,7 @@
 class jsonTest implements Serializable {
     private String jsonInput
-    def setJson(value) {
+    def call(value) {
         jsonInput = value
-    }
-    def getJson() {
 	def jsonSlurper = new JsonSlurper()
         def object = jsonSlurper.parseText(jsonInput)
         return object
