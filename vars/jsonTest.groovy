6609d50628694fa518c55791410e0620fb3e1383
crashGoBoom <chris.mundus@gmail.com>
2017-07-03 15:25:52 -0400

    try..THIS.

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -1,16 +1,7 @@
 import groovy.json.JsonSlurper
-import groovy.json.JsonOutput
 
-@NonCPS
 def call(value) {
-    def jsonSlurper = new JsonSlurper()
-    def object = jsonSlurper.parseText(value)
-    def otherInfo = otherLib('test')
-    def testObject = [name:"tito", age:43]
     sh "echo \"DNS.1 = tcp blah\" >> test.file"
     sh "cat test.file"
     sh "ls"
-    echo "resp from other lib: ${otherInfo.resp}"
-    echo "hi from json test: ${object.name}"
-    echo JsonOutput.toJson(testObject)
 }
