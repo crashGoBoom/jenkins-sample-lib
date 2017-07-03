e39852a483303cc21eaaeab940fd7c4f95480e94
crashGoBoom <chris.mundus@gmail.com>
2017-07-03 13:52:05 -0400

    Fixed

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -5,7 +5,7 @@ def call(value) {
     def jsonSlurper = new JsonSlurper()
     def object = jsonSlurper.parseText(value)
     def otherInfo = otherLib('test')
-    def testObject = {name:"tito", age:43}
+    def testObject = [name:"tito", age:43]
     echo "resp from other lib: ${otherInfo.resp}"
     echo "hi from json test: ${object.name}"
     echo JsonOutput.toJson(testObject)
