441fd51a22e425459edaa2633fa5d20115ca52fc
crashGoBoom <chris.mundus@gmail.com>
2017-07-03 13:49:46 -0400

    Added JsonOutput testing.

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -1,9 +1,12 @@
 import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
 
 def call(value) {
     def jsonSlurper = new JsonSlurper()
     def object = jsonSlurper.parseText(value)
     def otherInfo = otherLib('test')
+    def testObject = {name:"tito", age:43}
     echo "resp from other lib: ${otherInfo.resp}"
     echo "hi from json test: ${object.name}"
+    echo JsonOutput.toJson(testObject)
 }
