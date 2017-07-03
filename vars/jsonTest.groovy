4541caeef1eae7a1db3b43477126109af32a5412
crashGoBoom <chris.mundus@gmail.com>
2017-07-03 15:21:12 -0400

    Add sh test.

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -6,6 +6,8 @@ def call(value) {
     def object = jsonSlurper.parseText(value)
     def otherInfo = otherLib('test')
     def testObject = [name:"tito", age:43]
+    sh "echo \"DNS.1 = tcp blah\" >> test.file"
+    sh "cat test.file"
     echo "resp from other lib: ${otherInfo.resp}"
     echo "hi from json test: ${object.name}"
     echo JsonOutput.toJson(testObject)
