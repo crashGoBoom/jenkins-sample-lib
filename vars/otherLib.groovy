8b62651a6dccff339234a9b46e3eba9fc7863a41
crashGoBoom <chris.mundus@gmail.com>
2017-07-03 13:31:50 -0400

    Forgot something.

--- vars/otherLib.groovy
+++ vars/otherLib.groovy
@@ -1,6 +1,7 @@
 import groovy.json.JsonSlurper
 
 def call(value) {
+    def jsonSlurper = new JsonSlurper()
     def object = jsonSlurper.parseText('{ "resp": "Oh hi json from otherLib" }')
     echo "hi from other lib!"
     return object
