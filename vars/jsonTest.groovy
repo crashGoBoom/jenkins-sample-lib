ebcefed2173fcc47d5752516effd7d82fa716611
crashGoBoom <chris.mundus@gmail.com>
2017-07-03 11:21:04 -0400

    Testing more.

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -1,9 +1,7 @@
 import groovy.json.JsonSlurper
 
-class jsonTest implements Serializable {
-    def test(value) {
-	def jsonSlurper = new JsonSlurper()
-        def object = jsonSlurper.parseText(value)
-	echo "hi from json test: ${object.name}"
-    }
+def call(value) {
+    def jsonSlurper = new JsonSlurper()
+    def object = jsonSlurper.parseText(value)
+    echo "hi from json test: ${object.name}"
 }
