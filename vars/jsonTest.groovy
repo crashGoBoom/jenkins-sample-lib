e777a757f9de59bdbda936b5e699bb77658df657
crashGoBoom <chris.mundus@gmail.com>
2017-07-04 11:35:26 -0400

    Testing:

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -5,16 +5,11 @@ def call(value) {
     sh "echo ${value.file} >> test.file"
     sh "cat test.file"
 
-    def someJsonString = '{"person":{"name":"Guillaume","age":33,"pets":["dog","cat"]}}'
-    def json = parseJson(someJsonString)
-    sh "echo ${json.person.name} >> testSlurper.file"
-    sh "cat testSlurper.file"
-}
-
-def parseJson(jsonString) {
+    def jsonString = '{"person":{"name":"Guillaume","age":33,"pets":["dog","cat"]}}'
     def lazyMap = new JsonSlurper().parseText(jsonString)
     def m = [:]
     m.putAll(lazyMap)
     lazyMap = null
-    return m
+    sh "echo ${m.person.name} >> testSlurper.file"
+    sh "cat testSlurper.file"
 }
