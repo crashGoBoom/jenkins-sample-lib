ea6586a0221a361a556d0167d3489c1d2935b4d3
crashGoBoom <chris.mundus@gmail.com>
2017-07-04 12:01:45 -0400

    Woot

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -14,11 +14,11 @@ def call(value) {
     sh "echo ${value.file} >> test.file"
     sh "cat test.file"
 
-    def jsonString = '{"person":{"name":"Guillaume","age":33,"pets":["dog","cat"]}}'
+    def jsonString = '{"person":{"name":"Mundus","age":33,"pets":["dog","cat"]}}'
     def lazyMap = new JsonSlurper().parseText(jsonString)
     def newMap = new TestClass()
     newMap.copyProperties(lazyMap)
     lazyMap = null
-    sh "echo ${newMap} >> testSlurper.file"
+    sh "echo ${newMap.person.name} >> testSlurper.file"
     sh "cat testSlurper.file"
 }
