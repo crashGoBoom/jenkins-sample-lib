d9d635dc54a0eda5c021afa4876707c393fb6af3
crashGoBoom <chris.mundus@gmail.com>
2017-07-04 14:23:29 -0400

    Yep

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -1,33 +1,15 @@
-import groovy.json.JsonSlurper
-
-class NewClass implements Serializable {
-
-    Map<String,String> data
-
-    NewClass(map) {
-        map.each { key, value ->
-            data."$key" = value
-        }
-        map = null
-    }
-
-}
+import groovy.json.JsonSlurperClassic
 
 def call(value) {
     sh "echo ${value.file} >> test.file"
     sh "cat test.file"
-    def jsonString = '{"person":{"name":"Mundus","age":33,"pets":["dog","cat"]}}'
+    def jsonString = '{"person":{"name":"NotUs","age":33,"pets":["dog","cat"]}}'
     def newMap = parseJson(jsonString)
-    sh "echo ${newMap} >> testSlurper.file"
+    sh "echo ${newMap.person.name} >> testSlurper.file"
     sh "cat testSlurper.file"
 }
 
-@NonCPS
 def parseJson(jsonString) {
-    // Would like to use readJSON step, but it requires a context, even for parsing just text.
-    def lazyMap = new JsonSlurper().parseText(jsonString)
-    // JsonSlurper returns a non-serializable LazyMap, so copy it into a regular map before returning
-    def m = new NewClass(lazyMap)
-    lazyMap = null
-    return m
+    def lazyMap = new JsonSlurperClassic().parseText(jsonString)
+    return lazyMap
 }
