7c7ce6464a19357bb71fb4559ccb058356bb7ef3
crashGoBoom <chris.mundus@gmail.com>
2017-07-04 12:31:42 -0400

    Testing noncps function again.

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -1,28 +1,22 @@
 import groovy.json.JsonSlurper
 
-class TestClass implements Serializable {
-
-    def fields = [:]
-    def copyProperties(source) {
-        source.properties.each { key, value ->
-	    this.fields.put(key, value)
-        }
-    }
-
-    TestClass(lm) {
-        this.copyProperties(lm)
-    }
-}
-
 def call(value) {
 
     sh "echo ${value.file} >> test.file"
     sh "cat test.file"
-
     def jsonString = '{"person":{"name":"Mundus","age":33,"pets":["dog","cat"]}}'
-    def lazyMap = new JsonSlurper().parseText(jsonString)
-    def newMap = new TestClass(lazyMap)
-    lazyMap = null
-    sh "echo ${newMap.fields.person} >> testSlurper.file"
+    def newMap = parseJson(jsonString)
+    sh "echo ${newMap.person.name} >> testSlurper.file"
     sh "cat testSlurper.file"
+}
+
+@NonCPS
+def parseJson(jsonString) {
+    // Would like to use readJSON step, but it requires a context, even for parsing just text.
+    def lazyMap = new JsonSlurper().parseText(jsonString)
+    // JsonSlurper returns a non-serializable LazyMap, so copy it into a regular map before returning
+    def m = [:]
+    m.putAll(lazyMap)
+    lazyMap = null
+    return m
 }
