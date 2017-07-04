73de3880b099a3adace076c9c4384231edab3c26
crashGoBoom <chris.mundus@gmail.com>
2017-07-04 10:23:38 -0400

    Fix typeo.

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -4,6 +4,6 @@ import org.me.Options
 def call(value) {
     def x = new Options()
     x.file = value
-    sh "echo ${opts.file} >> test.file"
+    sh "echo ${x.file} >> test.file"
     sh "cat test.file"
 }
