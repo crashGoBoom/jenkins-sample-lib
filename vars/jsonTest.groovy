0bdf98560224b546b2cfb918518bb549da2adf9d
crashGoBoom <chris.mundus@gmail.com>
2017-07-04 11:30:21 -0400

    Added a json conversion test.

--- vars/jsonTest.groovy
+++ vars/jsonTest.groovy
@@ -1,8 +1,21 @@
 import groovy.json.JsonSlurper
 
 def call(value) {
-//    def x = new Options()
-//    x.file = value
+
     sh "echo ${value.file} >> test.file"
     sh "cat test.file"
+
+    def someJsonString = '{"person":{"name":"Guillaume","age":33,"pets":["dog","cat"]}}'
+    def json = parseJson(someJsonString)
+    sh "echo ${json.person.name} >> testSlurper.file"
+    sh "cat testSlurper.file"
+}
+
+@NonCPS
+def parseJson(jsonString) {
+    def lazyMap = new JsonSlurper().parseText(jsonString)
+    def m = [:]
+    m.putAll(lazyMap)
+    lazyMap = null
+    return m
 }
